92a775e32616eda35331f97297e7e398b90bdce0
Shai M <shaimsn@chardit.lan>
2018-03-07 01:48:12 -0800

    partial implementation of build_dataset.py

--- main/build_dataset.py
+++ main/build_dataset.py
@@ -27,8 +27,8 @@ from tqdm import tqdm
 SIZE = 64
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data_dir', default='data/SIGNS', help="Directory with the SIGNS dataset")
-parser.add_argument('--output_dir', default='data/64x64_SIGNS', help="Where to write the new data")
+parser.add_argument('--data_dir', default='data/GOPRO_Large', help="Directory with the GOPRO dataset")
+parser.add_argument('--output_dir', default='data/GOPRO_Dataset', help="Where to write the new data")
 
 
 def resize_and_save(filename, output_dir, size=SIZE):
@@ -45,45 +45,69 @@ if __name__ == '__main__':
     assert os.path.isdir(args.data_dir), "Couldn't find the dataset at {}".format(args.data_dir)
 
     # Define the data directories
-    train_data_dir = os.path.join(args.data_dir, 'train_signs')
-    test_data_dir = os.path.join(args.data_dir, 'test_signs')
+    train_data_dir = os.path.join(args.data_dir, 'train')
+    test_data_dir = os.path.join(args.data_dir, 'test')
 
-    # Get the filenames in each directory (train and test)
-    filenames = os.listdir(train_data_dir)
-    filenames = [os.path.join(train_data_dir, f) for f in filenames if f.endswith('.jpg')]
+    # Get the foldernames inside GOPRO_Large/train
+    train_foldernames = os.listdir(train_data_dir)
+    for foldername in train_foldernames:
+        train_subfolder_dir = os.path.join(train_data_dir, foldername)
 
-    test_filenames = os.listdir(test_data_dir)
-    test_filenames = [os.path.join(test_data_dir, f) for f in test_filenames if f.endswith('.jpg')]
+        subfolder_blur_dir = os.path.join(train_subfolder_dir, 'blur')
+        train_blur_filenames = os.listdir(subfolder_blur_dir)
+        train_blur_filenames = [os.path.join(subfolder_blur_dir, f) for f in train_blur_filenames if f.endswith('.png')]
+
+        subfolder_sharp_dir = os.path.join(train_subfolder_dir, 'sharp')
+        train_sharp_filenames = os.listdir(subfolder_sharp_dir)
+        train_sharp_filenames = [os.path.join(subfolder_sharp_dir, f) for f in train_sharp_filenames if f.endswith('.png')]
+
+    # Get the foldernames inside GOPRO_Large/test
+    test_foldernames = os.listdir(test_data_dir)
+    for foldername in test_foldernames:
+        test_subfolder_dir = os.path.join(test_data_dir, foldername)
+
+        subfolder_blur_dir = os.path.join(test_subfolder_dir, 'blur')
+        test_blur_filenames = os.listdir(subfolder_blur_dir)
+        test_blur_filenames = [os.path.join(subfolder_blur_dir, f) for f in test_blur_filenames if f.endswith('.png')]
+
+        subfolder_sharp_dir = os.path.join(test_subfolder_dir, 'sharp')
+        test_sharp_filenames = os.listdir(subfolder_sharp_dir)
+        test_sharp_filenames = [os.path.join(subfolder_sharp_dir, f) for f in test_sharp_filenames if f.endswith('.png')]
+
+    print("train_blur_filenames", train_blur_filenames); print("train_sharp_filenames", train_sharp_filenames)
+    print("test_blur_filenames", test_blur_filenames); print("test_sharp_filenames", test_sharp_filenames)
+    #so far, train_blur_filenames, train_sharp_filenames, test_blur_filenames and test_sharp_filenames lists created
 
     # Split the images in 'train_signs' into 80% train and 20% val
     # Make sure to always shuffle with a fixed seed so that the split is reproducible
-    random.seed(230)
-    filenames.sort()
-    random.shuffle(filenames)
-
-    split = int(0.8 * len(filenames))
-    train_filenames = filenames[:split]
-    val_filenames = filenames[split:]
-
-    filenames = {'train': train_filenames,
-                 'val': val_filenames,
-                 'test': test_filenames}
-
-    if not os.path.exists(args.output_dir):
-        os.mkdir(args.output_dir)
-    else:
-        print("Warning: output dir {} already exists".format(args.output_dir))
-
-    # Preprocess train, val and test
-    for split in ['train', 'val', 'test']:
-        output_dir_split = os.path.join(args.output_dir, '{}_signs'.format(split))
-        if not os.path.exists(output_dir_split):
-            os.mkdir(output_dir_split)
-        else:
-            print("Warning: dir {} already exists".format(output_dir_split))
-
-        print("Processing {} data, saving preprocessed data to {}".format(split, output_dir_split))
-        for filename in tqdm(filenames[split]):
-            resize_and_save(filename, output_dir_split, size=SIZE)
-
-    print("Done building dataset")
+    # random.seed(230)
+    #
+    # filenames.sort()
+    # random.shuffle(filenames)
+    #
+    # split = int(0.8 * len(filenames))
+    # train_filenames = filenames[:split]
+    # val_filenames = filenames[split:]
+    #
+    # filenames = {'train': train_filenames,
+    #              'val': val_filenames,
+    #              'test': test_filenames}
+    #
+    # if not os.path.exists(args.output_dir):
+    #     os.mkdir(args.output_dir)
+    # else:
+    #     print("Warning: output dir {} already exists".format(args.output_dir))
+    #
+    # # Preprocess train, val and test
+    # for split in ['train', 'val', 'test']:
+    #     output_dir_split = os.path.join(args.output_dir, '{}_pics'.format(split))
+    #     if not os.path.exists(output_dir_split):
+    #         os.mkdir(output_dir_split)
+    #     else:
+    #         print("Warning: dir {} already exists".format(output_dir_split))
+    #
+    #     print("Processing {} data, saving preprocessed data to {}".format(split, output_dir_split))
+    #     for filename in tqdm(filenames[split]):
+    #         resize_and_save(filename, output_dir_split, size=SIZE)
+    #
+    # print("Done building dataset")
--- main/model/data_loader.py
+++ main/model/data_loader.py
@@ -9,7 +9,7 @@ import torchvision.transforms as transforms
 # and http://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 # define a training image loader that specifies transforms on images. See documentation for more details.
 train_transformer = transforms.Compose([
-    transforms.Resize(64),  # resize the image to 64x64 (remove if images are already 64x64)
+    # transforms.Resize(64),  # resize the image to 64x64 (remove if images are already 64x64)
     transforms.RandomHorizontalFlip(),  # randomly flip image horizontally
     transforms.ToTensor()])  # transform it into a torch tensor
 

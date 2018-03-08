e0db323669b6973e6c140e218271e25ec01416d6
Shai M <shaimsn@DN0a22604d.SUNet>
2018-03-08 12:30:55 -0800

    changd train.py experiments source folder

--- main/train.py
+++ main/train.py
@@ -18,7 +18,7 @@ from evaluate import evaluate
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--data_dir', default='data/GOPRO_Dataset', help="Directory containing the dataset")
-parser.add_argument('--model_dir', default='experiments/base_model', help="Directory containing params.json")
+parser.add_argument('--model_dir', default='experiments/deblur_1', help="Directory containing params.json")
 parser.add_argument('--restore_file', default=None,
                     help="Optional, name of the file in --model_dir containing weights to reload before \
                     training")  # 'best' or 'train'

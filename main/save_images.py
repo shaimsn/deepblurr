b02c02f6f93ae8c544674a7a3c1e83cb3543dc55
Andy Gilbert <7andy121@gmail.com>
2018-03-11 15:58:21 -0700

    bugs

--- main/save_images.py
+++ main/save_images.py
@@ -69,7 +69,7 @@ def evaluate_save(model, loss_fn, dataloader, metrics, params, iter_num=0, model
             temp = output_batch[image].copy()
             temp = np.swapaxes(temp, 0, 2)
             temp = np.swapaxes(temp, 0, 1)
-            print('out_path = {}'.format(out_path))
+            # print('out_path = {}'.format(out_path))
             scipy.misc.imsave(out_path, temp)
 
     #     # compute all metrics on this batch
